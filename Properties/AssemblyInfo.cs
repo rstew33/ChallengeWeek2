667db4e2662ac5456ffd90903686807906838066
Andrew Steward <rstew33@gmail.com>
2020-11-21 16:08:52 -0500

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week2Challenges")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week2Challenges")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cad2d448-3cc7-45cb-8df6-396b0f9e919d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week2Challenge.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week2Challenges
+{
+    [TestClass]
+    public class Greeter
+    {
+    
+        public string Name(string inputName)
+        {
+            string fullName = "Hello " + inputName;
+            return fullName;
+        }
+
+        public string NameGoodbye(string inputname2)
+        {
+            string fullMessage = "Farewell " + inputname2;
+            return fullMessage;
+        }
+
+        public void TimeOfDay()
+        {
+            Console.WriteLine("Please enter the time of day (morning, afternoon, evening, night");
+            string userInput = Console.ReadLine().ToLower();
+            switch (userInput)
+            {
+                case "morning":
+                    Console.WriteLine("Good morning!");
+                    break;
+                case "afternoon":
+                    Console.WriteLine("Good afternoon!");
+                    break;
+                case "evening":
+                    Console.WriteLine("Good evening!");
+                    break;
+                case "night":
+                    Console.WriteLine("Good night!");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid input");
+                    break;
+            }
+        }
+
+        [TestMethod]
+        public void Main()
+        {
+            string fullName = Name("Jim");
+            string goodBye = NameGoodbye("Steve");
+            Console.WriteLine(fullName);
+            Console.WriteLine(goodBye);
+            TimeOfDay();
+
+
+        }
+
+    }
+    
+}
